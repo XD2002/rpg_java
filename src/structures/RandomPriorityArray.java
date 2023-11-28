6c3c7c5b1147c3a6356fa5941b6ce0f330cf5cbe
Xander Desoete <xander.desoete@ugent.be>
2023-11-28 22:39:31 +0100

    improved RandomPriorityArray

--- src/structures/RandomPriorityArray.java
+++ src/structures/RandomPriorityArray.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 public class RandomPriorityArray<T> {
     private final ArrayList<T>[] priorityArray;
+    private final int numPriorities;
     private final int triangleSize;
     private int size;
 
@@ -13,37 +14,29 @@ public class RandomPriorityArray<T> {
         for(int i=0; i<numPriorities; i++){
             priorityArray[i] = new ArrayList<>();
         }
+        this.numPriorities = numPriorities;
         triangleSize = numPriorities * (numPriorities + 1)/2;
-        System.out.println(triangleSize);
         size = 0;
     }
 
     public void add(T object, int priority){
+        if(priority < 0 || priority > numPriorities){
+            throw new IllegalArgumentException("Invalid priority");
+        }
         ArrayList<T> list = priorityArray[priority];
         list.add(object);
         size++;
     }
 
     public T getRandom(){
-        if(size == 0){
-            throw new RandomPriorityArrayEmptyException();
-        }
-        Random r = new Random();
-
-        int randomIndex = 1;
-        ArrayList<T> randomList = new ArrayList<>();
-
-        while(randomIndex > randomList.size()){
-            int randomLocTriangle= r.nextInt(triangleSize);
-            int randomPriority = (int) (Math.sqrt(8*randomLocTriangle+1)-1)/2;
-            randomList = priorityArray[randomPriority];
-        }
-
-        randomIndex = r.nextInt(randomList.size());
-        return randomList.get(randomIndex);
+        return get(false);
     }
 
     public T getRandomAndDelete(){
+        return get(true);
+    }
+
+    private T get(boolean delete){
         if(size == 0){
             throw new RandomPriorityArrayEmptyException();
         }
@@ -60,8 +53,10 @@ public class RandomPriorityArray<T> {
 
         randomIndex = r.nextInt(randomList.size());
         T object = randomList.get(randomIndex);
-        randomList.remove(object);
-        size--;
+        if(delete){
+            randomList.remove(object);
+            size--;
+        }
         return object;
     }
 }
